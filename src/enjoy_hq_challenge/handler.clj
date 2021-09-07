264b08fd6d720a54a76afd26b4a1cff9a8e7976f
sgurung <Infinity01>
2021-09-07 20:20:45 +0200

    some defualt handlers

--- src/enjoy_hq_challenge/handler.clj
+++ src/enjoy_hq_challenge/handler.clj
@@ -3,17 +3,23 @@
     [reitit.ring :as ring]
     ))
 
+(defn default-handlers []
+  (ring/create-default-handler
+    {:not-found          (constantly {:status 404 :body {:message "Endpoint not found"}})
+     :method-not-allowed (constantly {:status 405 :body {:message "Method not allowed"}})
+     :not-acceptable     (constantly {:status 406 :body {:message "Request not acceptable"}})}))
+
 (defn make-app []
   (ring/ring-handler
     (ring/router
       [
        ["/health-check" {:get {:handler (fn [_]
-                                          {:status 200
-                                           :body {:message "up"}
+                                          {:status  200
+                                           :body    {:message "up"}
                                            :headers {}})
                                }}]]
       )
-    (ring/create-default-handler)
+    (default-handlers)
     ))
 
 
@@ -21,5 +27,6 @@
   (def app (make-app))
 
   (app {:request-method :get :uri "/health-check"})
+  (app {:request-method :post :uri "/health-check"})
   (app {:request-method :get :uri "/lala"})
   )
